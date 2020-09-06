10baf3f3d8fe38d2cd6511563aadb35b1a52353f
Manan-lab <matshkalianm@gmail.com>
2020-09-06 22:00:17 +0400

    Homework 4

--- src/demo/Components/Price.js
+++ src/demo/Components/Price.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
 
-class Product extends Component{
+class Product extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            price: props.data,
+            rate : 478
+        }
+    }
 
+    changeCurrency = () => {
+        let {price,rate} = this.state;
+        let sign = price [price.length-1];
+        if (sign === "$"){
+           price = parseFloat(price)* rate + "֏"
+        }else if (sign === "֏"){
+            price = parseFloat(price) / rate + "$"
+        }
+        this.setState({
+            price:price
+        })
+    }
 
-    render(){
-        let {data} = this.props;
-        return(
-            <p>Price: {data}</p>
+    render() {
+        let { price } = this.state;
+        return (
+            <p>
+                Price: {price}
+                <button
+                onClick = {this.changeCurrency}
+                >
+                    AMD/USD
+                </button>
+            </p>
+
         );
     }
 }
