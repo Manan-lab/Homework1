63cb7bfa43b344e584174b2a3a3228296f7a5311
Manan-lab <matshkalianm@gmail.com>
2020-09-24 01:40:33 +0400

    HOMEWORK

--- src/App.js
+++ src/App.js
@@ -1,78 +1,17 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import Product from './demo/Components/Product'
-import Input from './demo/Components/Input'
+// import Product from './demo/Components/Product';
+// import Input from './demo/Components/Input';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ToDo from './demo/Components/Task/ToDo'
 
 
+function App() {
 
-function Productlist() {
-
-  return (
-    <div>
-      <Input />
-      <Product
-        name='banana'
-        price='1$'
-        description='Fresh bananas from Equador'
-      />
-
-      <Product
-        name="oranges"
-        price="2$"
-        description="Fresh oranges from Europ"
-      />
-
-    </div>
-
+  return(
+    <ToDo />
   );
-}
+};
 
-export default Productlist
-
-
-// function Hello(){
-//   let name = 'World'
-//   return(
-//     <div>
-//        <h1>Hello {name}</h1>
-//        <p>I'm a new Programmer</p>
-//        <p>Nice to meet you, ``)</p>
-//     </div>
-//   );
-// }
-//  function Person(props){
-//    return(
-//      <p>I am {props.age}</p>
-//    )
-
-//  }
-
-
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <Hello/>
-//         <Person age = '25' /> 
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
---
+++ src/demo/Components/Task/ToDo.js
@@ -0,0 +1,162 @@
+import React, { Component } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import idGenerator from '../helpers/idGenerator';
+import NewTask from './NewTask';
+import Task from './Task';
+import Confirm from './Confirm';
+import Modal from './Modal';
+
+class ToDo extends Component {
+    state = {
+        tasks: [],
+        checkedTasks: new Set(),
+        showConfirm: false,
+        editTask: null
+    };
+
+    addTask = (inputValue) => {
+        const tasks = [...this.state.tasks];
+
+        const newTask = {
+            id: idGenerator(),
+            text: inputValue
+        };
+
+        tasks.unshift(newTask)
+
+        this.setState({
+            tasks
+        });
+
+    };
+
+
+    removeTask = (taskId) => () => {
+
+        const newTasks = this.state.tasks.filter(task => task.id !== taskId);
+        this.setState({
+            tasks: newTasks
+        });
+    };
+
+    handleCheck = (taskId) => () => {
+        const checkedTasks = new Set(this.state.checkedTasks);
+        if (checkedTasks.has(taskId)) {
+            checkedTasks.delete(taskId);
+        }
+        else {
+            checkedTasks.add(taskId);
+        }
+        this.setState({ checkedTasks })
+    };
+
+    handleEdit = (task) => () => {
+        this.setState({ editTask: task });
+    };
+
+    onRemoveSelected = () => {
+        const checkedTasks = new Set(this.state.checkedTasks);
+        let tasks = [...this.state.tasks];
+
+        checkedTasks.forEach(taskId => {
+            tasks = tasks.filter(task => task.id !== taskId);
+        });
+
+        checkedTasks.clear();
+
+        this.setState({
+            tasks,
+            checkedTasks,
+            showConfirm: false
+        });
+    };
+
+    toggleConfirm = () => {
+        this.setState({
+            showConfirm: !this.state.showConfirm
+        });
+    };
+
+    handleSave = (taskId, value) => {
+
+        const tasks = [...this.state.tasks]
+
+        const taskIndex = tasks.findIndex(task => task.id === taskId)
+
+        tasks[taskIndex] = {
+            ...tasks[taskIndex],
+            text: value
+        };
+
+        this.setState({
+            tasks: tasks,
+            editTask: null
+        });
+    };
+
+    render() {
+        const { checkedTasks, tasks, showConfirm, editTask } = this.state
+
+        const tasksComponents = tasks.map((task) =>
+            <Col key={task.id}>
+                <Task
+                    data={task}
+                    onRemove={this.removeTask}
+                    onCheck={this.handleCheck(task.id)}
+                    onEdit={this.handleEdit(task)}
+                    disabled={!!checkedTasks.size}
+                />
+            </Col>
+        );
+
+        return (
+            <Container fluid={true}>
+                <Row >
+
+                    <Col md={{ span: 6, offset: 3 }}>
+                        <NewTask
+                            onAdd={this.addTask}
+                            disabled={!!checkedTasks.size}
+                        />
+                    </Col>
+
+                </Row>
+
+
+                <Row>
+                    {tasksComponents}
+                </Row>
+                <Row className='justify-content-center'>
+                    <Button
+                        variant="danger"
+                        disabled={!checkedTasks.size}
+                        onClick={this.toggleConfirm}
+                    >
+                        Remove selected
+                </Button>
+                </Row>
+
+                { showConfirm &&
+                    <Confirm
+                        count={checkedTasks.size}
+                        onSubmit={this.onRemoveSelected}
+                        onCancel={this.toggleConfirm}
+                    />
+                }
+                {!!editTask &&
+                    <Modal
+                        value={editTask}
+                        onSave={this.handleSave}
+                        onCancel={this.handleEdit(null)}
+                    />
+                }
+            </Container>
+        );
+    }
+}
+
+export default ToDo;
+
+
+
+
