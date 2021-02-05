996b68ee5ded9e679bcc8e9076060f0fba28a7ba
Manan-lab <matshkalianm@gmail.com>
2021-02-05 20:43:25 +0400

    emoji

--- src/Components/pages/Contact/Contact.js
+++ src/Components/pages/Contact/Contact.js
@@ -90,7 +90,7 @@ function ContactUs(props) {
 
                             <div className={styles.form}>
                                 <h1>Contact Us</h1>
-                                <p>Feel free to contact us any time.We will get back to you as soon as we can 😊</p>
+                                <p>Feel free to contact us any time.We will get back to you as soon as we can </p>
 
                                 <input
                                     type="name"
